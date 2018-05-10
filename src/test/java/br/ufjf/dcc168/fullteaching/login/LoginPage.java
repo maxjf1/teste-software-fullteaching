8777f099f215124adfe5ac7cab31d55eb24320f2
Rodolpho Rossete <rodolpho@ice.ufjf.br>
2018-05-09 21:20:42 -0300

    Novos teste de login adicionados

--- src/test/java/br/ufjf/dcc168/fullteaching/login/LoginPage.java
+++ src/test/java/br/ufjf/dcc168/fullteaching/login/LoginPage.java
@@ -1,8 +1,11 @@
 package br.ufjf.dcc168.fullteaching.login;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 
@@ -21,19 +24,46 @@ public class LoginPage {
     private WebElement login;
 
 
+    private WebElement failPanel;
+
+    private boolean logged;
+
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public LoginPage loginProfessor(String email, String password){
+    public LoginPage login(String email, String password){
 
         loginButton.click();
 
         this.email.sendKeys(email);
         this.password.sendKeys(password);
         login.click();
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, 3);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("form.col > app-error-message:nth-child(4)")));
+
+            this.failPanel = driver.findElement(By.cssSelector("form.col > app-error-message:nth-child(4)"));
+           this.logged =false;
+        }catch(Exception e){
+            this.logged =true;
+            System.out.println(e.getMessage());
+        }
+
+
 
         return this;
 
+
+
+    }
+
+    public WebElement getFailPanel(){
+        return this.failPanel;
+    }
+
+    public boolean isLogged() {
+        return logged;
     }
 }
